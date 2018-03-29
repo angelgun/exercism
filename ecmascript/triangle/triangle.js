9d54872e3dc63f95d23c7af642ac0b69524a0ea1
angelgun <angelyoon82@gmail.com>
2018-03-29 17:16:50 +0900

    Refactoring triangle.js

--- ecmascript/triangle/triangle.js
+++ ecmascript/triangle/triangle.js
@@ -1,22 +1,28 @@
 class Triangle {
-  constructor(a, b, c) {
-    this.triangle = [a, b, c].sort((a, b) => {
-      return a - b;
-    });
+  constructor(...sides) {
+    this.triangle = [...sides].sort((a, b) => a - b);
   }
 
   kind() {
-    const a = this.triangle[0];
-    const b = this.triangle[1];
-    const c = this.triangle[2];
+    const side1 = this.triangle[0];
+    const side2 = this.triangle[1];
+    const larget = this.triangle[2];
 
-    if (a < 0) throw '길이가 음수는 없습니다.';
-    if (a * b * c === 0) throw '길이가 0일수는 없습니다.';
-    if (a + b < c) throw '삼각형이 아닙니다.';
+    // if (a < 0) throw new Error('길이가 음수는 없습니다.');
+    // if (a * b * c === 0) throw new Error('길이가 0일수는 없습니다.');
 
-    if (a === b && b === c) {
+    this.triangle.forEach(item => {
+      if (item === 0) throw new Error('길이가 0일수는 없습니다.');
+      if (item < 0) throw new Error('길이가 음수는 없습니다.');
+    });
+
+    if (side1 + side2 < larget) throw new Error('삼각형이 아닙니다.');
+
+    const triangleSideSet = new Set(this.triangle);
+
+    if (triangleSideSet.size === 1) {
       return 'equilateral';
-    } else if (a === b || b === c) {
+    } else if (triangleSideSet.size === 2) {
       return 'isosceles';
     } else {
       return 'scalene';
